c95ed9e3922b91e5223afe9c49655e2f97af5205
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-02-25 23:29:35 +0530

    Footer Text added

--- car-listing/src/App.js
+++ car-listing/src/App.js
@@ -1,11 +1,51 @@
 
 import './App.css';
-
+import { BrowserRouter, Link } from "react-router-dom";
+import FooterComponent from './FooterComponent/FooterComponent';
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome React</h1>
-    </div>
+    <BrowserRouter>
+      <div className='grid-container'>
+        <header className='header'>
+          <div className='side-bar'>
+          <button>&#9776;</button>
+          </div>
+          <div className="app-logo">
+            <Link to="/">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/b/b8/CARS24_Official_New_Logo.png"
+                alt="CARS25"
+              />
+            </Link>
+          </div>
+          <div className="navigation-links">
+            <Link to="/">
+              Go to Cart
+              <i
+                className="fa fa-shopping-cart fa-2x mx-2 btn "
+                style={{ color: "white" }}
+              ></i>
+            </Link>
+            <Link to="/">
+              Sign In
+              <i
+                className="fa fa-user p-3 fa-6x mx-2 btn "
+                style={{ color: "white" }}
+              ></i>
+            </Link>
+          </div>
+        </header>
+        <main>
+        <h1>Popular Cities</h1>
+        </main>
+        <footer className="footer-area">
+          <FooterComponent />
+        </footer>
+      </div>
+     <h1>Welcome Ract</h1>
+  
+    </BrowserRouter>
+   
   );
 }
 
---
+++ car-listing/src/FooterComponent/FooterComponent.js
@@ -0,0 +1,130 @@
+import React from "react";
+import "./styles.css";
+
+const FooterComponent = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col col-md-3 col-sm-3 col-xl-3">
+          <h3 className="heading">Keep in Touch</h3>
+          <p className="paragraph">
+            Experience CARS24 App on Mobile
+            <span>Powered by CARS24</span>
+          </p>
+        </div>
+        <div className="col col-md-3 col-sm-3 col-xl-3">
+          <h3 className="heading">Useful Links</h3>
+          <ul className="mb-2">
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Contact Us</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">About Us</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Privacy Policy</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">
+                Terms and Conditions
+              </a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">FAQ</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Site Map</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Blog</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Branches</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">CARS24 Finance</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Investors</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Partners</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col col-md-3 col-sm-3 col-xl-3">
+          <h3 className="heading">Top Brands</h3>
+          <ul className="mb-2">
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Maruthi Suzuki</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Honda</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Tata</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Toyota</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Ford</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Mahindra</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Skoda</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Flat</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Nissan</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Renault</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">AUDI</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col col-md-3 col-sm-3 col-xl-3">
+          <h3 className="heading">Top Cities</h3>
+          <ul className="mb-2">
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">New Delhi</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Hyderabad</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Bangalore</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Mumbai</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Chennai</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Pune</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Kochi</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Kolkata</a>
+            </li>
+            <li className="list">
+              <a href="https://www.cars24.com/buy-used-cars/">Noida</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterComponent;
