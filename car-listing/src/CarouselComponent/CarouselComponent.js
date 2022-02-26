5d69a83448ab901ff8fef206f3746ef92913abc6
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-02-26 22:41:16 +0530

    Carousel Component changes

--- car-listing/src/CarouselComponent/CarouselComponent.js
+++ car-listing/src/CarouselComponent/CarouselComponent.js
@@ -1,25 +1,50 @@
 import React from "react";
 import "./Carouselstyles.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
+import "bootstrap/dist/css/bootstrap.css";
+import Carousel from "react-bootstrap/Carousel";
 
 const CarouselComponent = () => {
   return (
+    <div className="carousel-part">
+      <h2 className="carousel-h2">OUR TOP SERVICES</h2>
       <div className="Carousel-area">
-    <Carousel showArrows autoPlay showThumbs={false}>
-      <div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3duq9lRneBtz_bAW9gISMun3qpWPf2VtH1g&usqp=CAU" alt="Carousel 1" />
-        <p className="legend">Legend 1</p>
+        <Carousel fade>
+          <Carousel.Item interval={300}>
+            <img
+              className="d-block w-100"
+              src="https://www.medianews4u.com/wp-content/uploads/2020/10/CARS24-launches-new-ad-campaign-%E2%80%98Duniya-Boli-Lagayegi.jpg"
+              alt=" One"
+            />
+            <Carousel.Caption>
+              <h3>Let Us Finance Your Dream Car</h3>
+              <p>Zero Down Payment</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={300}>
+            <img
+              className="d-block w-100"
+              src="https://cdn.dealstreetasia.com/uploads/2021/09/CARS24-Delivery-Truck-1-e1632116038616.jpg?fit=979,543"
+              alt=" Two"
+            />
+            <Carousel.Caption>
+              <h3>A Whole World of Cars</h3>
+              <p>Find Your Perfect Ride</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={300}>
+            <img
+              className="d-block w-100"
+              src="https://failurebeforesuccess.com/wp-content/uploads/2022/02/CARS11622486_oOH_800x400CARS24_1080wide-800x395-1.png"
+              alt=" Two"
+            />
+            <Carousel.Caption>
+              <h3>Book a Test Drive</h3>
+              <p>We Deliver To Your Doorstep</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
-      <div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT94k0iZv-7BvS5K6IsjjXrAr2lC5dy92XbKg&usqp=CAU" alt="Carousel 2" />
-        <p className="legend">Legend 2</p>
-      </div>
-      <div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT94k0iZv-7BvS5K6IsjjXrAr2lC5dy92XbKg&usqp=CAU" alt="Carousel 3" />
-        <p className="legend">Legend 3</p>
-      </div>
-    </Carousel>
     </div>
   );
 };
